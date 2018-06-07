94d7d06f3014fb757426c1224d22ba6aeacd756a
hedgehedge <37985424+hedgehedge@users.noreply.github.com>
2018-06-07 23:22:06 +0200

    hmm merg (#19)

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -17,7 +17,7 @@ namespace openshserv.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "hmm_bef more value one ch_1 plus some change";
+            return "hmm_bef more value one ch_1 plus some change hmm merg";
         }
 
         // POST api/values
